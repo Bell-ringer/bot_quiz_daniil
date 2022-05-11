3401ac8540cd0063788d836f7bb9e506802416a8
DigitalConcern <mihanik1608@gmail.com>
2022-05-12 00:37:51 +0300

    initial commit

---
+++ root.py
@@ -0,0 +1,18 @@
+from aiogram.types import CallbackQuery, ParseMode
+
+from aiogram_dialog import ChatEvent
+from aiogram_dialog.widgets.input import MessageInput
+from aiogram_dialog.widgets.kbd import Button, Select, Back, Column, Cancel, Url, SwitchTo
+from aiogram_dialog.widgets.text import Format
+
+from database import Questions
+from config import categories
+
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.types import Message
+
+from aiogram_dialog import Dialog, DialogManager, Window, StartMode
+from aiogram_dialog.manager.protocols import LaunchMode
+from aiogram_dialog.widgets.kbd import Start
+from aiogram_dialog.widgets.text import Const
+
