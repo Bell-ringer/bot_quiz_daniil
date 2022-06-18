0c237fcaa531845a46f3e65fc942497de1d6a764
Михаил Никифоров <Mihanik1608@hotmail.com>
2022-06-18 17:56:36 +0300

    test1 diagram

--- test1.py
+++ test1.py
@@ -6,6 +6,7 @@ from aiogram.types import CallbackQuery, ParseMode
 
 from aiogram_dialog import ChatEvent
 from aiogram_dialog.widgets.kbd import Button, Select, Cancel, Row
+from aiogram_dialog.widgets.media import StaticMedia
 from aiogram_dialog.widgets.text import Format, Jinja
 
 from aiogram.dispatcher.filters.state import StatesGroup, State
@@ -18,7 +19,7 @@ from database import Situations, Results
 from bot import MyBot
 from graph import graph
 
-next_state: State
+next_state = {}
 
 
 class test1SG(StatesGroup):
@@ -57,6 +58,7 @@ class test1SG(StatesGroup):
     c3s4 = State()
     c8s4 = State()
     answer = State()
+    finish = State()
 
 
 # функция для получения данных из состояний
@@ -66,11 +68,20 @@ async def get_data(dialog_manager: DialogManager, **kwargs):
         'end_time': dialog_manager.current_context().dialog_data.get("end_time", None),
         'answer_buffer': dialog_manager.current_context().dialog_data.get("answer_buffer", ""),
         'answer_variants': dialog_manager.current_context().dialog_data.get("answer_variants", ""),
+        "not_last": dialog_manager.current_context().dialog_data.get("not_last", ""),
+        "last": dialog_manager.current_context().dialog_data.get("last", ""),
+        "filename": dialog_manager.current_context().dialog_data.get("filename", ""),
+        "next_button_text": dialog_manager.current_context().dialog_data.get("next_button_text", ""),
     }
 
 
 async def next_question(c: CallbackQuery, button: Button, manager: DialogManager):
-    await manager.dialog().switch_to(next_state)
+    if manager.current_context().dialog_data["last"]:
+        next_state[c.from_user.id] = test1SG.c1s1
+        os.remove("results_" + str(c.from_user.id) + ".png")
+        await manager.done()
+    else:
+        await manager.dialog().switch_to(next_state[c.from_user.id])
 
 
 async def get_random_answers(manager: DialogManager, situation: int):
@@ -94,8 +105,13 @@ async def start_test(c: CallbackQuery, button: Button, manager: DialogManager):
     manager.current_context().dialog_data["start_time"] = datetime.now().strftime("%d-%m-%y %H:%M:%S")
     manager.current_context().dialog_data["user_id"] = c.from_user.id
     manager.current_context().dialog_data["name"] = c.from_user.first_name
+    manager.current_context().dialog_data["not_last"] = True
+    manager.current_context().dialog_data["last"] = False
+    manager.current_context().dialog_data["next_button_text"] = "Следующий вопрос ⏩"
 
     manager.current_context().dialog_data["try_num"] = await Results.filter(user_id=c.from_user.id).count() + 1
+
+    next_state[c.from_user.id] = test1SG.c1s1
 
     await get_random_answers(manager, 1)
     await manager.dialog().switch_to(test1SG.c1s1)
@@ -109,7 +125,7 @@ async def c1s1_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 2)
 
     global next_state
-    next_state = test1SG.c1s2
+    next_state[c.from_user.id] = test1SG.c1s2
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -121,7 +137,7 @@ async def c1s2_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 3)
 
     global next_state
-    next_state = test1SG.c5s4
+    next_state[c.from_user.id] = test1SG.c5s4
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -133,7 +149,7 @@ async def c5s4_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 4)
 
     global next_state
-    next_state = test1SG.c5s2
+    next_state[c.from_user.id] = test1SG.c5s2
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -145,8 +161,8 @@ async def c5s2_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 5)
 
     global next_state
-    next_state = test1SG.c5s1
-    await manager.dialog().switch_to(next_state)
+    next_state[c.from_user.id] = test1SG.c5s1
+    await manager.dialog().switch_to(next_state[c.from_user.id])
 
 
 async def c5s1_handler(c: ChatEvent, select: Select, manager: DialogManager, item_id: str):
@@ -157,7 +173,7 @@ async def c5s1_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 6)
 
     global next_state
-    next_state = test1SG.c4s4
+    next_state[c.from_user.id] = test1SG.c4s4
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -169,7 +185,7 @@ async def c4s4_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 7)
 
     global next_state
-    next_state = test1SG.c6s2
+    next_state[c.from_user.id] = test1SG.c6s2
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -181,7 +197,7 @@ async def c6s2_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 8)
 
     global next_state
-    next_state = test1SG.c7s2
+    next_state[c.from_user.id] = test1SG.c7s2
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -193,7 +209,7 @@ async def c7s2_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 9)
 
     global next_state
-    next_state = test1SG.c7s3
+    next_state[c.from_user.id] = test1SG.c7s3
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -205,7 +221,7 @@ async def c7s3_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 10)
 
     global next_state
-    next_state = test1SG.c1s5
+    next_state[c.from_user.id] = test1SG.c1s5
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -217,8 +233,8 @@ async def c1s5_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 11)
 
     global next_state
-    next_state = test1SG.c1s3
-    await manager.dialog().switch_to(next_state)
+    next_state[c.from_user.id] = test1SG.c1s3
+    await manager.dialog().switch_to(next_state[c.from_user.id])
 
 
 async def c1s3_handler(c: ChatEvent, select: Select, manager: DialogManager, item_id: str):
@@ -229,7 +245,7 @@ async def c1s3_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 12)
 
     global next_state
-    next_state = test1SG.c3s1
+    next_state[c.from_user.id] = test1SG.c3s1
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -241,7 +257,7 @@ async def c3s1_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 13)
 
     global next_state
-    next_state = test1SG.c3s2_2
+    next_state[c.from_user.id] = test1SG.c3s2_2
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -253,7 +269,7 @@ async def c3s2_1_handler(c: ChatEvent, select: Select, manager: DialogManager, i
     await get_random_answers(manager, 14)
 
     global next_state
-    next_state = test1SG.c3s3
+    next_state[c.from_user.id] = test1SG.c3s3
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -265,7 +281,7 @@ async def c3s2_2_handler(c: ChatEvent, select: Select, manager: DialogManager, i
     await get_random_answers(manager, 15)
 
     global next_state
-    next_state = test1SG.c3s3
+    next_state[c.from_user.id] = test1SG.c3s3
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -277,7 +293,7 @@ async def c3s3_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 16)
 
     global next_state
-    next_state = test1SG.c2s1
+    next_state[c.from_user.id] = test1SG.c2s1
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -289,7 +305,7 @@ async def c2s1_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 17)
 
     global next_state
-    next_state = test1SG.c4s3
+    next_state[c.from_user.id] = test1SG.c4s3
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -301,7 +317,7 @@ async def c4s3_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 18)
 
     global next_state
-    next_state = test1SG.c4s2
+    next_state[c.from_user.id] = test1SG.c4s2
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -313,7 +329,7 @@ async def c4s2_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 19)
 
     global next_state
-    next_state = test1SG.c2s2
+    next_state[c.from_user.id] = test1SG.c2s2
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -325,7 +341,7 @@ async def c2s2_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 20)
 
     global next_state
-    next_state = test1SG.c6s3
+    next_state[c.from_user.id] = test1SG.c6s3
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -337,7 +353,7 @@ async def c6s3_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 21)
 
     global next_state
-    next_state = test1SG.c4s1
+    next_state[c.from_user.id] = test1SG.c4s1
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -349,7 +365,7 @@ async def c4s1_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 22)
 
     global next_state
-    next_state = test1SG.c6s1
+    next_state[c.from_user.id] = test1SG.c6s1
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -361,7 +377,7 @@ async def c6s1_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 23)
 
     global next_state
-    next_state = test1SG.c2s4
+    next_state[c.from_user.id] = test1SG.c2s4
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -373,7 +389,7 @@ async def c2s4_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 24)
 
     global next_state
-    next_state = test1SG.c2s3
+    next_state[c.from_user.id] = test1SG.c2s3
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -385,7 +401,7 @@ async def c2s3_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 25)
 
     global next_state
-    next_state = test1SG.c6s4
+    next_state[c.from_user.id] = test1SG.c6s4
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -397,7 +413,7 @@ async def c6s4_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 26)
 
     global next_state
-    next_state = test1SG.c7s4
+    next_state[c.from_user.id] = test1SG.c7s4
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -409,7 +425,7 @@ async def c7s4_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 27)
 
     global next_state
-    next_state = test1SG.c7s5
+    next_state[c.from_user.id] = test1SG.c7s5
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -421,7 +437,7 @@ async def c7s5_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 28)
 
     global next_state
-    next_state = test1SG.c5s5
+    next_state[c.from_user.id] = test1SG.c5s5
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -433,7 +449,7 @@ async def c5s5_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 29)
 
     global next_state
-    next_state = test1SG.c8s3
+    next_state[c.from_user.id] = test1SG.c8s3
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -445,7 +461,7 @@ async def c8s3_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 30)
 
     global next_state
-    next_state = test1SG.c8s1
+    next_state[c.from_user.id] = test1SG.c8s1
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -457,7 +473,7 @@ async def c8s1_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 31)
 
     global next_state
-    next_state = test1SG.c8s2
+    next_state[c.from_user.id] = test1SG.c8s2
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -469,7 +485,7 @@ async def c8s2_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 32)
 
     global next_state
-    next_state = test1SG.c3s4
+    next_state[c.from_user.id] = test1SG.c3s4
     await manager.dialog().switch_to(test1SG.answer)
 
 
@@ -481,12 +497,14 @@ async def c3s4_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
     await get_random_answers(manager, 33)
 
     global next_state
-    next_state = test1SG.c8s4
+    next_state[c.from_user.id] = test1SG.c8s4
     await manager.dialog().switch_to(test1SG.answer)
 
 
 async def c8s4_handler(c: ChatEvent, select: Select, manager: DialogManager, item_id: str):
     manager.current_context().dialog_data["c8s4"] = manager.current_context().dialog_data["answer_stuff"][item_id][2]
+    manager.current_context().dialog_data["not_last"] = False
+    manager.current_context().dialog_data["last"] = True
 
     manager.current_context().dialog_data["end_time"] = datetime.now().strftime("%d-%m-%y %H:%M:%S")
 
@@ -509,22 +527,9 @@ async def c8s4_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
 
     await graph(c1=c1, c2=c2, c3=c3, c4=c4, c5=c5, c6=c6, c7=c7, c8=c8, user_id=c.from_user.id)
 
-    await MyBot.bot.send_photo(c.from_user.id,
-                               open("results_" + str(c.from_user.id) + ".png", "rb"),
-                               caption='Поздравляю!\nНа этом внеурочное занятие завершено. Спасибо за твои решения!\n'
-                                       'Желаю тебе достичь всех амбициозных целей! Удачи тебе!\n'
-                                       f'Время начала теста:\n{manager.current_context().dialog_data["start_time"]}\n'
-                                       f'Время окончания теста:\n{manager.current_context().dialog_data["end_time"]}\n'
-                                       'Твои результаты:\n'
-                                       f'Компетенция №1: {c1}%\n'
-                                       f'Компетенция №2: {c2}%\n'
-                                       f'Компетенция №3: {c3}%\n'
-                                       f'Компетенция №4: {c4}%\n'
-                                       f'Компетенция №5: {c5}%\n'
-                                       f'Компетенция №6: {c6}%\n'
-                                       f'Компетенция №7: {c7}%\n'
-                                       f'Компетенция №8: {c8}%\n'
-                               )
+    manager.current_context().dialog_data["filename"] = "results_" + str(c.from_user.id) + ".png"
+
+    manager.current_context().dialog_data["answer_buffer"] = f'Поздравляю!\nНа этом внеурочное занятие завершено. Спасибо за твои решения!\nЖелаю тебе достичь всех амбициозных целей! Удачи тебе!\nВремя начала теста:\n{manager.current_context().dialog_data["start_time"]}\nВремя окончания теста:\n{manager.current_context().dialog_data["end_time"]}\nТвои результаты:\nКомпетенция №1: {c1}%\nКомпетенция №2: {c2}%\nКомпетенция №3: {c3}%\nКомпетенция №4: {c4}%\nКомпетенция №5: {c5}%\nКомпетенция №6: {c6}%\nКомпетенция №7: {c7}%\nКомпетенция №8: {c8}%\n'
 
     await Results.create(user_id=manager.current_context().dialog_data["user_id"],
                          try_num=manager.current_context().dialog_data["try_num"],
@@ -540,15 +545,21 @@ async def c8s4_handler(c: ChatEvent, select: Select, manager: DialogManager, ite
                          c7=c7,
                          c8=c8)
 
-    os.remove("results_" + str(c.from_user.id) + ".png")
+    manager.current_context().dialog_data["next_button_text"] = "Завершить тест!"
 
-    await manager.done()
+    await manager.dialog().switch_to(test1SG.answer)
+
+    # await manager.done()
 
 
 test1 = Dialog(
     Window(
+        StaticMedia(
+            path="{filename}",
+            when="last"
+        ),
         Format("{answer_buffer}"),
-        Button(Const("Следующий вопрос ⏩"), id="next_question", on_click=next_question),
+        Button(Format("{next_button_text}"), id="next_question", on_click=next_question),
         getter=get_data,
         state=test1SG.answer
     ),
